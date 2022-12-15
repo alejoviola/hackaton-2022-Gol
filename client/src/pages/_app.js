9553ca0ae4315820c9c798088d7c654ff5df236e
= <=>
2022-12-15 16:11:48 -0300

    [update] delete ; from _app

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -30,7 +30,7 @@ export default function App({ Component, pageProps }) {
                 })}
                 chains={chains}
             >
-                <Component {...pageProps} />;
+                <Component {...pageProps} />
             </RainbowKitProvider>
         </WagmiConfig>
     );
