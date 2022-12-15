f757ab2280e01584c4a39ae71ff9b05ec180f320
= <=>
2022-12-15 15:39:54 -0300

    [new] Rainbowkit installed

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,37 @@
-import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { darkTheme, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import '../styles/globals.css';
+
+const { chains, provider } = configureChains([mainnet, polygon, optimism, arbitrum], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains,
+});
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider
+                theme={darkTheme({
+                    accentColor: '#ea580c',
+                })}
+                chains={chains}
+            >
+                <Component {...pageProps} />;
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
 }
